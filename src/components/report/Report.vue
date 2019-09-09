b504218bb16efff8edc7a0307abff381c756bfa1
唐思敏 <978443154@qq.com>
2019-09-09 22:40:42 +0800

    完成报表功能开发

---
+++ src/components/report/Report.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item to="/welcome">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片区域 -->
+    <el-card>
+          <!-- 为ECharts准备一个具备大小（宽高）的Dom -->
+    <div id="main" style="width: 600px;height:400px;"></div>
+    </el-card>
+    </div>
+</template>
+<script>
+// 导入echarts组件
+import echarts from 'echarts'
+import _ from 'lodash'
+export default {
+    data(){
+        return {
+            options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+        }
+    },
+    created(){
+
+    },
+    // 此时页面上的元素已经渲染完毕
+    async mounted(){
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('main'));
+        const{data:res}=await this.$http.get('reports/type/1')
+        if(res.meta.status!==200) return this.$message.error('获取折线图失败')
+        // 准备数据和配置项
+        const result=_.merge(res.data,this.options)
+       // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(result);
+
+    },
+    methods:{
+
+    }
+}
+</script>
+<style lang="less">
+
+</style>
